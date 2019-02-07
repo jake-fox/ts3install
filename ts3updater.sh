47fdb79ba0126d0a8a72a3a4061c5f0d08169001
jake-fox <47289805+jake-fox@users.noreply.github.com>
2019-02-07 22:50:38 +0800

    Update ts3updater.sh

--- ts3updater.sh
+++ ts3updater.sh
@@ -4,6 +4,9 @@
 # Version: 1.5
 # Description: Installs and updates TeamSpeak 3 servers
 # License: MIT License
+
+apt install tar
+
 cd "$(dirname "$0")" || exit 1
 
 # check whether the dependencies curl, jq, and tar are installed
@@ -102,7 +105,7 @@ if [ "$old_version" != "$version" ]; then
 		if [ -e 'ts3server_startscript.sh' ]; then
         		./ts3server_startscript.sh stop
 		else
-			mkdir "$tsdir" || { echo 'Could not create installation directory. If you wanted to upgrade an existing installation, make sure to place this script INSIDE the existing installation directory.' 1>&2; rm "$tmpfile"; exit 1; }
+			sudo mkdir "$tsdir" || { echo 'Could not create installation directory. If you wanted to upgrade an existing installation, make sure to place this script INSIDE the existing installation directory.' 1>&2; rm "$tmpfile"; exit 1; }
 			cd "$tsdir" && mv ../"$(basename "$0")" .
 		fi
 
