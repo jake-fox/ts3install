2f8e6ede9d59686d52ad3bfc918e8ab29f2f560b
jake-fox <47289805+jake-fox@users.noreply.github.com>
2019-03-16 20:40:32 +0800

    Update ts3install.sh

--- ts3install.sh
+++ ts3install.sh
@@ -12,14 +12,35 @@ touch .ts3server_license_accepted
 
 
 echo type a password for query serveradmin 
-
 read pass
 
-
 echo type a queryport for server query
-
 read query
+echo type a voiceport for the server
+read voiceport
 
-./ts3server_startscript.sh start serveradmin_password=$pass & query_port=$query
+touch /home/teamspeak3-server_linux_amd64/.ts3server.ini
+
+echo
+"machine_id=
+default_voice_port=$voiceport
+voice_ip=0.0.0.0
+licensepath=/etc/teamspeak3-server/
+filetransfer_port=30033
+filetransfer_ip=0.0.0.0
+query_port=10011
+query_ip=0.0.0.0
+query_ip_whitelist=/var/lib/teamspeak3-server/query_ip_whitelist.txt
+query_ip_blacklist=/var/lib/teamspeak3-server/query_ip_blacklist.txt
+dbplugin=ts3db_sqlite3
+dbpluginparameter=
+dbsqlpath=/usr/share/teamspeak3-server/sql/
+dbsqlcreatepath=create_sqlite/
+dblogkeepdays=90
+logpath=/var/log/teamspeak3-server
+logquerycommands=0
+dbclientkeepdays=30" > /home/teamspeak3-server_linux_amd64/.ts3server.ini
+
+./ts3server_startscript.sh start serveradmin_password=$pass
 
 
