0c06badfd444a953b916e8539ac6bba2476799df
jake-fox <47289805+jake-fox@users.noreply.github.com>
2019-07-31 12:05:15 +0800

    Update ts3install.sh

--- ts3install.sh
+++ ts3install.sh
@@ -3,9 +3,9 @@
 
 #teamspeak install script
 cd /home
-sudo wget https://files.teamspeak-services.com/releases/server/3.6.1/teamspeak3-server_linux_amd64-3.6.1.tar.bz2
-tar -xvf teamspeak3-server_linux_amd64-3.6.1.tar.bz2
-rm -f teamspeak3-server_linux_amd64-3.6.1.tar.bz2
+sudo wget https://files.teamspeak-services.com/releases/server/3.9.1/teamspeak3-server_linux_amd64-3.9.1.tar.bz2
+tar -xvf teamspeak3-server_linux_amd64-3.9.1.tar.bz2
+rm -f teamspeak3-server_linux_amd64-3.9.1.tar.bz2
 cd /home/teamspeak3-server_linux_amd64
 chmod +x ./ts3server_startscript.sh
 touch .ts3server_license_accepted
