f0077f55f3ee22a4c0479898880a4b1b5c838946
jake-fox <47289805+jake-fox@users.noreply.github.com>
2026-02-16 11:07:57 +1000

    Update ts3install.sh

--- ts3install.sh
+++ ts3install.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 apt update -y && apt upgrade -y
-
+cd
 wget https://files.teamspeak-services.com/releases/server/3.13.7/teamspeak3-server_linux_amd64-3.13.7.tar.bz2
 
 sudo apt -y install lbzip2
 
 tar -xf teamspeak3-server_linux_amd64-3.13.7.tar.bz2
-cd teamspeak3-server_linux_amd64
-cp * -R /home/teamspeak
-cd
-rm -r teamspeak3-server_linux_amd64
+
 rm teamspeak3-server_linux_amd64-3.13.7.tar.bz2
+
+cd teamspeak3-server_linux_amd64
+
 touch /root/teamspeak3-server_linux_amd64/.ts3server_license_accepted
 
 sudo tee /lib/systemd/system/ts3server.service > /dev/null << 'EOF'
